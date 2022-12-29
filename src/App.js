7a93dc31726d0d0fb9c1eb9d9bc9dae752be3fd4
Kiervey Maghanoy <kierveymaghanoy24@gmail.com>
2022-12-29 23:00:07 +0800

    fix import files; remove github pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import Main from './components/Main';
-import Favorites from './components/favorites/Favorites';
-import Meal from './components/meals/Meal';
+import Main from './components/Main.js';
+import Favorites from './components/favorites/Favorites.js';
+import Meal from './components/meals/Meal.js';
 
 const App = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/themealrecipe" element={<Main />} />
+                <Route path="/" element={<Main />} />
                 <Route path="/favorites" element={<Favorites />} />
                 <Route path="/meal/:id" element={<Meal />} />
             </Routes>
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -12,7 +12,7 @@ const Navigation = () => {
     return (
         <div className="d-flex__space__between header">
             <h2 style={{textAlign: 'center'}}>
-                <Link to='/themealrecipe'>The Meal Recipe</Link>
+                <Link to='/'>The Meal Recipe</Link>
             </h2>
             { 
                 route.pathname !== '/favorites' 
